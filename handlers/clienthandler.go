36ad47e148d647d518299c51367a007b2844f856
Nathan Bass <sblood86@gmail.com>
2016-02-25 18:07:58 -0600

    First pass at message parsing.

--- handlers/clienthandler.go
+++ handlers/clienthandler.go
@@ -23,6 +23,10 @@ func ClientHandler(conn *websocket.Conn) {
 
 	alert, err := json.Marshal(types.AlertMin{Response: 200})
 	if err != nil {
+		/* Uncertain if this should be fatal or not, invalid
+		 * operation on the server side should definitely cause
+		 * some form of error presentation to the administrator
+		 * but I'm uncertain about full shutdown. */
 		log.Fatalln(err)
 	}
 
@@ -37,5 +41,33 @@ func ClientHandler(conn *websocket.Conn) {
 
 		// TODO proper message handling
 		fmt.Printf("%s\n", p)
+
+		var message types.MasterObj
+		if err := json.Unmarshal(p, &message); err != nil {
+			log.Println(err)
+			return
+		}
+
+		switch {
+		case message.Action == "msg":
+			/* TODO parse the destination and if the destination exists
+			 * send the message (should work for 1to1 even if the user is
+			 * not currently online); if destination doesn't exist return an
+			 * error (for now just return the message itself for testing).
+			 */
+			client.Conn.WriteMessage(websocket.BinaryMessage, p)
+			break
+		default:
+			errmin, err := json.Marshal(types.ErrorMin{Response: 400})
+			if err != nil {
+				/* Uncertain if this should be fatal or not, invalid
+				 * operation on the server side should definitely cause
+				 * some form of error presentation to the administrator
+				 * but I'm uncertain about full shutdown. */
+				log.Fatalln(err)
+			}
+			client.Conn.WriteMessage(websocket.BinaryMessage, errmin)
+			break
+		}
 	}
 }
---
+++ types/masterobj.go
@@ -0,0 +1,15 @@
+package types
+
+// MasterObj for unmarshalling all client objects on the server side.
+type MasterObj struct {
+	// Found in all objects
+	Action string `json:"action"`
+	// Only found in message objects
+	To   string `json:"to"`
+	From string `json:"from"`
+	Body string `json:"message"`
+	// Found in join/part messages
+	Room string `json:"room"`
+	// Found only in authentication messages
+	User []byte `json:"user"`
+}
