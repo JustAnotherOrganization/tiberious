5ac795695fe23ffa62349b688fb88b6db8071ea1
IngCr3at1on <sblood86@gmail.com>
2016-03-23 14:35:52 -0500

    First pass at 1toMany messaging
    Also fixes a bug in evaluation causing client messages to be
    ignored if a previous message resulted in an error respose

---
+++ handlers/roomhandler.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"tiberious/types"
+)
+
+var (
+	rooms = make(map[string]types.Room)
+)
+
+// GetRoom check if a room exists and if so return it
+func GetRoom(rname string) (bool, types.Room) {
+	var rexists = false
+	var room types.Room
+	for k, r := range rooms {
+		if rname == k {
+			room = r
+			rexists = true
+			break
+		}
+	}
+
+	return rexists, room
+}
+
+// GetNewRoom should only be used if the room doesn't already exist
+func GetNewRoom(rname string) types.Room {
+	room := make(types.Room)
+	rooms[rname] = room
+	return room
+}
