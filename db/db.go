83dcbc12abf4f8924c9831f49d76a82a84f7056e
IngCr3at1on <sblood86@gmail.com>
2016-09-29 01:16:33 -0500

    First pass at refactoring db package

--- db/db.go
+++ db/db.go
@@ -1,40 +1,36 @@
 package db
 
 import (
+	"log"
 	"strings"
 	"tiberious/settings"
 	"tiberious/types"
-
-	"github.com/pborman/uuid"
 )
 
-/*
-Data map:
-	Redis:
-		Users:
-			Main Key: "user-"+<user-type>+"-"+<loginname>+<uuid> (hash)
-			Joined Rooms: "user-"+<user-type+"-"+<uuid>+"rooms" (set)
-			Joined Groups: "user-"+<user-type+"-"+<uuid>+"groups" (set)
-		Rooms:
-			Info: "room-"+<group name>+<room name>+"-info" (hash)
-			User List: "room-"+<group name>+<room name>+"-list" (set)
-		Groups:
-			Info: "group-"+<group name>+"info" (hash)
-			User List: "group-"+<group name>+"-users" (set)
-			Room List: "group-"+<group name>+"-rooms" (set)
-*/
-
-var config settings.Config
+var (
+	config settings.Config
+	rdis   rdisClient
+)
 
 func init() {
 	config = settings.GetConfig()
+
+	if config.UserDatabase == 0 {
+		// Load Redis DB
+		var err error
+		rdis, err = newRedisClient()
+		if err != nil {
+			log.Fatalln("Unable to connect to redis database:", err)
+		}
+		log.Println("User database started on redis db", config.RedisUser)
+	}
 }
 
 // GetKeySet returns all the keys that match a given search pattern.
 func GetKeySet(search string) ([]string, error) {
 	switch {
 	case config.UserDatabase == 0:
-		return rdis.Keys(search).Result()
+		return rdis.getKeySet(search)
 	default:
 		break
 	}
@@ -46,27 +42,12 @@ func GetKeySet(search string) ([]string, error) {
 func WriteUserData(user *types.User) error {
 	switch {
 	case config.UserDatabase == 0:
-		if err := rdis.HMSet(
-			"user-"+user.Type+"-"+user.LoginName+"-"+user.ID.String(),
-			"id", user.ID.String(),
-			"type", user.Type,
-			"username", user.Username,
-			"loginname", user.LoginName,
-			"email", user.Email,
-			"password", user.Password,
-			"salt", user.Salt,
-			"connected", strbool(user.Connected),
-		).Err(); err != nil {
-			return err
-		}
-
-		go updateSet("user-"+user.Type+"-"+user.ID.String()+"-rooms", user.Rooms)
-		go updateSet("user-"+user.Type+"-"+user.ID.String()+"-groups", user.Groups)
-		break
+		return rdis.writeUserData(user)
 	default:
 		break
 	}
 
+	// TODO log this, it should not occur
 	return nil
 }
 
@@ -74,21 +55,12 @@ func WriteUserData(user *types.User) error {
 func WriteRoomData(room *types.Room) error {
 	switch {
 	case config.UserDatabase == 0:
-		if err := rdis.HMSet("room-"+room.Group+"-"+room.Title+"-info", "title", room.Title, "group", room.Group, "private", strbool(room.Private)).Err(); err != nil {
-			return err
-		}
-
-		var slice []string
-		for _, u := range room.Users {
-			slice = append(slice, u.ID.String())
-		}
-
-		go updateSet("room-"+room.Group+"-"+room.Title+"-list", slice)
-		break
+		return rdis.writeRoomData(room)
 	default:
 		break
 	}
 
+	// TODO log this, it should not occur
 	return nil
 }
 
@@ -96,24 +68,7 @@ func WriteRoomData(room *types.Room) error {
 func WriteGroupData(group *types.Group) error {
 	switch {
 	case config.UserDatabase == 0:
-		if err := rdis.HSet("group-"+group.Title+"-info", "title", group.Title).Err(); err != nil {
-			return err
-		}
-
-		var slice []string
-		for _, r := range group.Rooms {
-			slice = append(slice, r.Title)
-		}
-		go updateSet("group-"+group.Title+"-rooms", slice)
-
-		slice = nil
-		for _, u := range group.Users {
-			slice = append(slice, u.ID.String())
-		}
-
-		go updateSet("group-"+group.Title+"-users", slice)
-
-		break
+		return rdis.writeGroupData(group)
 	default:
 		break
 	}
@@ -123,115 +78,51 @@ func WriteGroupData(group *types.Group) error {
 
 // UserExists returns whether a user exists in the database.
 func UserExists(id string) (bool, error) {
-	switch {
-	case config.UserDatabase == 0:
-		res, err := GetKeySet("user-*-*-" + id)
-		if err != nil {
-			return false, nil
-		}
-
-		if len(res) == 0 {
-			return false, nil
-		}
-
-		return true, nil
-	default:
-		break
+	res, err := GetKeySet("user-*-*-" + id)
+	if err != nil {
+		return false, nil
 	}
 
-	return false, nil
+	if len(res) == 0 {
+		return false, nil
+	}
+
+	return true, nil
 }
 
 // RoomExists returns whether a room exists in the database.
 func RoomExists(gname, rname string) (bool, error) {
-	switch {
-	case config.UserDatabase == 0:
-		res, err := GetKeySet("room-" + gname + "-" + rname + "*")
-		if err != nil {
-			return false, err
-		}
-
-		if len(res) == 0 {
-			return false, nil
-		}
-
-		return true, nil
-	default:
-		break
+	res, err := GetKeySet("room-" + gname + "-" + rname + "*")
+	if err != nil {
+		return false, err
 	}
 
-	return false, nil
+	if len(res) == 0 {
+		return false, nil
+	}
+
+	return true, nil
 }
 
 // GroupExists returns whether a group exists in the database.
 func GroupExists(gname string) (bool, error) {
-	switch {
-	case config.UserDatabase == 0:
-		res, err := GetKeySet("group-" + gname + "-*")
-		if err != nil {
-			return false, err
-		}
-
-		if len(res) == 0 {
-			return false, nil
-		}
-
-		return true, nil
-	default:
-		break
+	res, err := GetKeySet("group-" + gname + "-*")
+	if err != nil {
+		return false, err
 	}
 
-	return false, nil
+	if len(res) == 0 {
+		return false, nil
+	}
+
+	return true, nil
 }
 
 // GetUserData gets all the data for a given user ID from the database
 func GetUserData(id string) (*types.User, error) {
 	switch {
 	case config.UserDatabase == 0:
-		user := new(types.User)
-
-		keys, err := GetKeySet("user-*-*-" + id)
-		if err != nil {
-			return nil, err
-		}
-
-		if len(keys) == 0 {
-			return nil, nil
-		}
-
-		info, err := rdis.HGetAllMap(keys[0]).Result()
-		if err != nil {
-			return nil, err
-		}
-
-		user.ID = uuid.Parse(info["id"])
-		user.Type = info["type"]
-		user.Username = info["username"]
-		user.LoginName = info["loginname"]
-		user.Email = info["email"]
-		user.Password = info["password"]
-		user.Salt = info["salt"]
-		user.Connected = boolstr(info["connected"])
-
-		rooms, err := rdis.SMembers("user-" + user.Type + "-" + user.ID.String() + "-rooms").Result()
-		if err != nil {
-			return nil, err
-		}
-
-		if len(rooms) > 0 {
-			user.Rooms = rooms
-		}
-
-		groups, err := rdis.SMembers("user-" + user.Type + "-" + user.ID.String() + "-groups").Result()
-		if err != nil {
-			return nil, err
-		}
-
-		if len(groups) > 0 {
-			user.Groups = groups
-		}
-
-		return user, nil
+		return rdis.getUserData(id)
 	default:
 		break
 	}
@@ -243,35 +134,7 @@ func GetUserData(id string) (*types.User, error) {
 func GetRoomData(gname, rname string) (*types.Room, error) {
 	switch {
 	case config.UserDatabase == 0:
-		room := new(types.Room)
-		room.Users = make(map[string]*types.User)
-
-		info, err := rdis.HGetAllMap("room-" + gname + "-" + rname + "-info").Result()
-		if err != nil {
-			return nil, err
-		}
-
-		room.Title = info["title"]
-		room.Group = info["group"]
-		room.Private = boolstr(info["private"])
-
-		users, err := rdis.SMembers("room-" + gname + "-" + rname + "-list").Result()
-		if err != nil {
-			return nil, err
-		}
-
-		room.Users = make(map[string]*types.User)
-		if len(users) > 0 {
-			for _, v := range users {
-				u, err := GetUserData(v)
-				if err != nil {
-					return nil, err
-				}
-				room.Users[u.ID.String()] = u
-			}
-		}
-
-		return room, nil
+		return rdis.getRoomData(gname, rname)
 	default:
 		break
 	}
@@ -283,50 +146,7 @@ func GetRoomData(gname, rname string) (*types.Room, error) {
 func GetGroupData(gname string) (*types.Group, error) {
 	switch {
 	case config.UserDatabase == 0:
-		group := new(types.Group)
-
-		group.Title = gname
-		group.Rooms = make(map[string]*types.Room)
-		group.Users = make(map[string]*types.User)
-
-		users, err := rdis.SMembers("group-" + gname + "-users").Result()
-		if err != nil {
-			return nil, err
-		}
-
-		if len(users) > 0 {
-			for _, v := range users {
-				/* For some reason the length of this keeps coming up as 1 above
-				 * the actual number of entries so confirm it's not nil before
-				 * attempting to run GetUserData with the given string. */
-				if v != "" {
-					u, stat := GetUserData(v)
-					if stat != nil {
-						return nil, stat
-					}
-					group.Users[u.ID.String()] = u
-				}
-			}
-		}
-
-		rooms, err := rdis.SMembers("group-" + gname + "-rooms").Result()
-		if err != nil {
-			return nil, err
-		}
-
-		if len(rooms) > 0 {
-			for _, v := range rooms {
-				if v != "" {
-					r, err := GetRoomData(gname, v)
-					if err != nil {
-						return nil, err
-					}
-					group.Rooms[r.Title] = r
-				}
-			}
-		}
-
-		return group, nil
+		return rdis.getGroupData(gname)
 	default:
 		break
 	}
@@ -334,8 +154,8 @@ func GetGroupData(gname string) (*types.Group, error) {
 	return nil, types.NotInDB
 }
 
-/*DeleteUser removes a user from all rooms and groups and deletes it from the
- * database (user sparingly). */
+// DeleteUser removes a user from all rooms and groups and deletes it from the
+// database (use sparingly). */
 func DeleteUser(user *types.User) error {
 	for _, gname := range user.Groups {
 		group, err := GetGroupData(gname)
@@ -364,13 +184,7 @@ func DeleteUser(user *types.User) error {
 
 	switch {
 	case config.UserDatabase == 0:
-		if err := rdis.Del("user-" + user.Type + "-" + user.ID.String() + "-groups").Err(); err != nil {
-			return err
-		}
-		if err := rdis.Del("user-" + user.Type + "-" + user.ID.String() + "-rooms").Err(); err != nil {
-			return err
-		}
-		return rdis.Del("user-" + user.Type + "-" + user.LoginName + "-" + user.ID.String()).Err()
+		return rdis.deleteUser(user)
 	default:
 		break
 	}
