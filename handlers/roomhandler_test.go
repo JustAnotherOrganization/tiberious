5061ca615d5709fa3bed23804ec9942dce43b866
jeremywho <jeremy@weebu.com>
2016-10-15 19:21:41 -0700

    moved handler tests to gingko/gomega framework

--- handlers/roomhandler_test.go
+++ handlers/roomhandler_test.go
@@ -1,87 +1,92 @@
 package handlers
 
 import (
-	"testing"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 
 	"tiberious/types"
 )
 
-func TestGetGroup(t *testing.T) {
+var _ = Describe("group handlers", func() {
 	var g *types.Group
-	// No database is enabled, should return nil
-	g = GetGroup("#test")
-	if g != nil {
-		t.Fail()
-	}
 
-	// #default exists with or with out a database
-	g = GetGroup("#default")
-	if g == nil {
-		t.Fail()
-	}
+	Describe("Calling GetGroup on #test", func() {
+		It("No database is enabled, should return nil", func() {
+			g = GetGroup("#test")
+			Expect(g).To(BeNil())
+		})
+	})
 
-	if g.Title != "#default" {
-		t.Fail()
-	}
-}
+	Describe("Calling GetGroup on #default", func() {
+		It("#default exists with or with out a database", func() {
+			g = GetGroup("#default")
+			Expect(g).NotTo(BeNil())
+			Expect(g.Title).To(Equal("#default"))
+		})
+	})
+})
 
-func TestGetNewGroup(t *testing.T) {
+var _ = Describe("new group handlers", func() {
 	var g *types.Group
-	// No database is enabled, should return nil
-	g = GetNewGroup("#anything", false)
-	if g != nil {
-		t.Fail()
-	}
 
-	g = GetNewGroup("#test", true)
-	if g == nil {
-		t.Fail()
-	}
+	Describe("Calling GetNewGroup on #test", func() {
+		It("", func() {
+			g = GetNewGroup("#anything")
+			Expect(g).NotTo(BeNil())
+			Expect(g.Title).To(Equal("#anything"))
+		})
+	})
 
-	if g.Title != "#test" {
-		t.Fail()
-	}
-}
+	Describe("Calling GetNewGroup on #test", func() {
+		It("should get group", func() {
+			g = GetNewGroup("#test")
+			Expect(g).NotTo(BeNil())
+			Expect(g.Title).To(Equal("#test"))
+		})
+	})
+})
 
-func TestGetRoom(t *testing.T) {
+var _ = Describe("group room", func() {
 	var r *types.Room
-	/* No database is enabled so only the #default and #test groups exist
-	 * test having been created during the previous test. */
-	r = GetRoom("#anything", "#anywhere")
-	if r != nil {
-		t.Fail()
-	}
 
-	// Only the room #general exists current...
-	r = GetRoom("#default", "#anywhere")
-	if r != nil {
-		t.Fail()
-	}
+	Describe("Calling GetRoom on #anything, #anywhere", func() {
+		It("No database is enabled so only the #default and #test groups exist", func() {
+			r = GetRoom("#anything", "#anywhere")
+			Expect(r).To(BeNil())
+		})
+	})
 
-	r = GetRoom("#default", "#general")
-	if r == nil {
-		t.Fail()
-	}
+	Describe("Calling GetRoom on #default, #anywhere", func() {
+		It("#anywhere should not exist", func() {
+			r = GetRoom("#default", "#anywhere")
+			Expect(r).To(BeNil())
+		})
+	})
 
-	if r.Title != "#general" {
-		t.Fail()
-	}
-}
+	Describe("Calling GetRoom on #default, #general", func() {
+		It("#default/#general exists by default", func() {
+			r = GetRoom("#default", "#general")
+			Expect(r).NotTo(BeNil())
+			Expect(r.Title).To(Equal("#general"))
+		})
+	})
+})
 
-func TestGetNewRoom(t *testing.T) {
+var _ = Describe("new room", func() {
 	var r *types.Room
-	// Only the #general and #test groups exist.
-	r = GetNewRoom("#anything", "#anywhere")
-	if r != nil {
-		t.Fail()
-	}
 
-	r = GetNewRoom("#default", "#anywhere")
-	if r == nil {
-		t.Fail()
-	}
+	Describe("Calling GetNewRoom on #anything, #anywhere", func() {
+		It("#anything group does not exist", func() {
+			r = GetNewRoom("#anything", "#anywhere")
+			Expect(r).To(BeNil())
+		})
+	})
 
-	if r.Title != "#anywhere" {
-		t.Fail()
-	}
-}
+	Describe("Calling GetRoom on #default, #anywhere", func() {
+		It("#default exists by default", func() {
+			r = GetNewRoom("#default", "#anywhere")
+			Expect(r).NotTo(BeNil())
+			Expect(r.Title).To(Equal("#anywhere"))
+		})
+	})
+})
