20f8118fa41e6c6ecc12d053f90658974833fb07
IngCr3at1on <sblood86@gmail.com>
2016-12-26 18:38:16 -0600

    Add a connection handler
    This starts the client handler which in turn starts the dbclient and group
    handler

--- main.go
+++ main.go
@@ -1,47 +1,17 @@
 package main
 
 import (
-	"net/http"
+	"io"
 	"os"
 
-	"tiberious/handlers/client"
+	"tiberious/handlers/connection"
 	"tiberious/settings"
-	"tiberious/types"
 
 	"github.com/Sirupsen/logrus"
-	"github.com/gorilla/websocket"
 )
 
-var (
-	config *settings.Config
-	log    *logrus.Logger
-
-	clientHandler client.Handler
-)
-
-// TODO move this into a separate handler of some form.
-func newConnection(w http.ResponseWriter, r *http.Request) {
-	var upgrader = websocket.Upgrader{
-		ReadBufferSize:  config.ReadBufferSize,
-		WriteBufferSize: config.WriteBufferSize,
-	}
-
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		if _, ok := err.(websocket.HandshakeError); ok {
-			w.WriteHeader(404)
-			w.Write([]byte("Invalid websocket handshake"))
-			return
-		}
-		logrus.Error(err)
-		return
-	}
-
-	go clientHandler.HandleConnection(conn)
-}
-
-func init() {
-	log = logrus.New()
+func main() {
+	log := logrus.New()
 
 	// Errors or warnings encountered when initializing settings will always
 	// print to stdout because we don't know if there's a log file set in the
@@ -52,32 +22,26 @@ func init() {
 	}
 	log.Println(note)
 
-	config = settings.GetConfig()
+	config := settings.GetConfig()
 
 	// At this point we have our config so set the log location in logrus (if
 	// found in config).
 	if config.Log != "" {
-		writer, err := os.OpenFile(config.Log, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		var writer io.Writer
+		writer, err = os.OpenFile(config.Log, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		log.Out = writer
 	}
-}
 
-func main() {
-	config = settings.GetConfig()
-	var err error
-	clientHandler, err = client.NewHandler(config, make(map[string]*types.Client), log)
+	connectionHandler, err := connection.NewHandler(config, log)
 	if err != nil {
-		log.Error(err)
+		log.Fatal(err)
 	}
 
-	http.HandleFunc("/", http.NotFound)
-	http.HandleFunc("/ws", newConnection)
-	log.Infof("Starting Tiberious on %s", config.Port)
-	if err := http.ListenAndServe(config.Port, nil); err != nil {
-		log.Error(err)
+	if err := connectionHandler.ListenAndServe(); err != nil {
+		log.Fatal(err)
 	}
 }
